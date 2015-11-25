22946e88d7abcf86dce35d67d464da6136e20ef0
Aaron C. Meadows <shdwjk@gmail.com>
2015-11-24 21:33:29 -0600

    Added Grunt for example building, added repeating.

--- TheAaronSheet.js
+++ TheAaronSheet.js
@@ -6,25 +6,249 @@
 var TAS = TAS || (function(){
     'use strict';
 
-    var version = '0.1.0',
-        lastUpdate = 1447856102,
+    var version = '0.1.1',
+        lastUpdate = 1448405980,
 
-    unique = function (arr) {
-        var a = [],i,l;
-        for (i=0, l=arr.length; i<l; i++) {
-            if (a.indexOf(arr[i]) === -1 && arr[i] !== '') {
-                a.push(arr[i]);
+		queuedUpdates = {},
+
+	log = function(){
+        _.each(arguments,function(m){
+            switch(typeof m){
+                case 'string':
+                    console.log('<TAS> :: '+m);
+                    break;
+                case 'number':
+                    console.log('<TAS> :: [number]: '+m);
+                    break;
+                default:
+                    console.log('<TAS> :: ['+typeof m +']:');
+                    console.log(m);
+                    console.log('<TAS> :: =========================================================');
             }
-        }
-        return a;
-    },
-    uniqueToLower = function(arr) {
-            return unique(arr.map(function(s){ return (s.toLowerCase && s.toLowerCase());}));
-    },
+        });
+	},
+
+//	each = function(set,cb,ctx){
+//		var idx,len,keys;
+//		if(isIterable(set)) {
+//			for (idx = 0, len = set.length; idx<len; ++idx) {
+//				cb.apply(ctx,[set[idx],idx,set]);
+//			}
+//		} else {
+//			keys = Object.keys(set);
+//			for (idx = 0, len = keys.length; idx<len; ++idx) {
+//				cb.apply(ctx,[set[keys[idx]],keys[idx],set]);
+//			}
+//		}
+//		return set;
+//	},
+//
+//	map = function(set,cb,ctx){
+//		var idx,len,keys,results=[];
+//		if(isIterable(set)) {
+//			for (idx = 0, len = set.length; idx<len; ++idx) {
+//				results.push(cb.apply(ctx,[set[idx],idx,set]));
+//			}
+//		} else {
+//			keys = Object.keys(set);
+//			for (idx = 0, len = keys.length; idx<len; ++idx) {
+//				results.push(cb.apply(ctx,[set[keys[idx]],keys[idx],set]));
+//			}
+//		}
+//		return results;
+//	},
+//
+//	reduce = function(set,cb,memo,ctx){
+//		var idx,len,keys,m = memo;
+//		if(isIterable(set)) {
+//			for (idx = 0, len = set.length; idx<len; ++idx) {
+//				m = cb.apply(ctx,[m,set[idx],idx,set]);
+//			}
+//		} else {
+//			keys = Object.keys(set);
+//			for (idx = 0, len = keys.length; idx<len; ++idx) {
+//				m = cb.apply(ctx,[m,set[keys[idx]],keys[idx],set]);
+//			}
+//		}
+//		return m;
+//	},
+
+	namesFromArgs = function(args,base){
+        return _.chain(args)
+            .reduce(function(memo,attr){
+			if(_.isArray(args) || _.isArguments(args)){
+                memo = namesFromArgs(attr,memo);
+			} else if('string' === typeof attr) {
+				memo.push(attr);
+			}
+            return memo;
+		},(_.isArray(base) && base) || [])
+        .uniq()
+        .value();
+	},
+
+	addId = function(obj,value){
+		Object.defineProperty(obj,'id',{
+			value: value,
+			writeable: false,
+			enumerable: false
+		});
+	},
+
+	addProp = function(obj,prop,value,fullname){
+		(function(){
+            var pname=prop,
+			    full_pname = fullname || prop,
+                pvalue=value;
+			Object.defineProperty(obj, pname, {
+				set: function(v){
+					var o = {};
+					pvalue=v;
+					o[full_pname]=v;
+					setAttrs(o);
+					
+					log('setting '+pname+'['+full_pname+'] to value '+v);
+				},
+				get: function(){
+					return pvalue;
+				}
+			});
+		}());
+	},
+	
+	/*
+
+	 TAS.repeating('inventory')
+		.attrs(['strength'])
+		.fields(['qty','weight']
+		.reduce(function(memo,row,attrs,id,full){
+			return memo;
+		},
+		function(result,attrs,full){
+			
+		},0)
+		.map(function(row,attrs,id,full){
+			//stuff
+		})
+		.execute()
+
+	 TAS.repeating('inventory')
+		.columns(['qty','weight','cost'])  // or .fields or .column or .field
+		.operation({
+			sum: {func: function(memo, fields){return memo+(fields.qty*fields.weight)}, memo: 0},
+			subtotal: function(memo, fields){return fields.qty*fields.weight;}
+			})
+		.set({
+				{func: 'sum', attr: 'total_weight', fields: ['qty','weight']},
+				{func: 'sum', attr: 'total_cost', fields: ['qty','cost']},
+				{func: 'subtotal', attr: { row: 'total_weight'}, fields: ['qty','weight']},
+				{func: 'subtotal', attr: { row: 'total_cost'}, fields: ['qty','cost']}
+			})
+		.execute();
+	 
+	 */
+
+
+	repeating = function( section ) {
+		return (function(s){
+			var sectionName = s,
+				attrNames = [],
+				fieldNames = [],
+				operations = [],
+			
+			repAttrs = function(){
+				attrNames = namesFromArgs(arguments,attrNames);
+				return this;
+			},
+			repFields = function(){
+				fieldNames = namesFromArgs(arguments,fieldNames);
+				return this;
+			},
+			repReduce = function(func, memo, final, context) { 
+				operations.push({type: 'reduce', func: func, memo: memo, final: final, context:context});
+				return this;
+			},
+			repMap = function(func, final, context) {
+				operations.push({type: 'map', func: func, final: final, context:context});
+				return this;
+			},
+			repExecute = function(){
+				var rowSet = {},
+					attrSet = {},
+					fieldIds = [],
+					fullFieldNames = [];
+
+				// begin collection of attrs
+				// collect attrs
+				// collect fields
+				// build row set
+				// call each operation per row.
+				// call each operation's final
+				getSectionIDs("repeating_"+sectionName,function(ids){
+					var request = [];
+					fieldIds = ids;
+					fullFieldNames = _.reduce(fieldIds,function(memo,id){
+						return memo.concat(_.map(fieldNames,function(name){
+							return 'repeating_'+sectionName+'_'+id+'_'+name;  
+						}));
+					},[]);
+					getAttrs( _.uniq(attrNames.concat(fullFieldNames)), function(values){
+						_.each(attrNames,function(aname){
+							if(values.hasOwnProperty(aname)){
+								addProp(attrSet,aname,values[aname]);
+							}
+						});
+
+						rowSet = _.reduce(fieldIds,function(memo,id){
+							var r={};
+							addId(r,id);
+							_.each(fieldNames,function(name){
+								var fn = 'repeating_'+sectionName+'_'+id+'_'+name;  
+								addProp(r,name,values[fn],fn);
+							});
+
+							memo[id]=r;
+
+							return memo;
+						},{});
+
+                        debugger;
+
+
+
+					});
+				});
+				// finalize attrs
+			};
+				
+			return {
+				attrs: repAttrs,
+				attr: repAttrs,
+
+				column: repFields,
+				columns: repFields,
+				field: repFields,
+				fields: repFields,
+
+				reduce: repReduce,
+				inject: repReduce,
+				foldl: repReduce,
+
+				map: repMap,
+				each: repMap,
+				collect: repMap,
+
+
+				execute: repExecute,
+				go: repExecute,
+				run: repExecute
+			};
+		}(section));
+	},
+
 
     repeatingSimpleSum = function(section, field, destination){
         getSectionIDs("repeating_"+section,function(ids){
-            ids = uniqueToLower(ids);
             getAttrs( ids.map(function(i){
                 return 'repeating_'+section+'_'+i+'_'+field;  
             }),function(res){
@@ -43,11 +267,8 @@ var TAS = TAS || (function(){
     console.log('-=> TheAaronSheet v'+version+' <=-  ['+(new Date(lastUpdate*1000))+']');
 
     return {
-        util: {
-            unique: unique,
-            uniqueToLower: uniqueToLower
-        },
-        repeatingSimpleSum: repeatingSimpleSum
+        repeatingSimpleSum: repeatingSimpleSum,
+		repeating: repeating
     };
 }());
 
